7f812fbec3526db581fa5c8aecf0fdc3e0b00777
nilsonsasaki <nilsonsasaki@gmail.com>
2021-11-06 22:28:00 -0300

    Updated MainActivity.kt into a fragment host and moved it to ui package

---
+++ app/src/main/java/com/nilsonsasaki/moviesapp/ui/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.nilsonsasaki.moviesapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.nilsonsasaki.moviesapp.R
+import com.nilsonsasaki.moviesapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+}
