45db4032d912dff0bf63452a3c87095f9384279b
nilsonsasaki <nilsonsasaki@gmail.com>
2021-11-09 19:19:32 -0300

    added onSupportNavigateUp

--- app/src/main/java/com/nilsonsasaki/moviesapp/ui/MainActivity.kt
+++ app/src/main/java/com/nilsonsasaki/moviesapp/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nilsonsasaki.moviesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -10,12 +11,13 @@ import com.nilsonsasaki.moviesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navHostFragment = supportFragmentManager
@@ -23,5 +25,9 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
 
         setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
