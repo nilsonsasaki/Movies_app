ea9b8a6c583c416425b7d401f3603c213600ef85
nilsonsasaki <nilsonsasaki@gmail.com>
2021-11-09 19:16:12 -0300

    created MoviesListAdapter.kt

---
+++ app/src/main/java/com/nilsonsasaki/moviesapp/ui/adapters/MoviesListAdapter.kt
@@ -0,0 +1,49 @@
+package com.nilsonsasaki.moviesapp.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.net.toUri
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.nilsonsasaki.moviesapp.databinding.ListItemBinding
+import com.nilsonsasaki.moviesapp.network.MoviesListItem
+import com.nilsonsasaki.moviesapp.ui.adapters.MoviesListAdapter.MoviesListViewHolder
+
+
+class MoviesListAdapter(private val list: List<MoviesListItem>,
+private val onItemClicked: (MoviesListItem)-> Unit) :
+    RecyclerView.Adapter<MoviesListViewHolder>() {
+
+    class MoviesListViewHolder(private var binding: ListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(moviesListItem: MoviesListItem) {
+            binding.tvTitle.text = moviesListItem.title
+            binding.ivCover.load(moviesListItem.coverUrl.toUri())
+            binding.tvRating.text = moviesListItem.rating.toString()
+            binding.tvReleaseYear.text = moviesListItem.releaseDate
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup, viewType: Int
+    ): MoviesListViewHolder {
+        val viewHolder= MoviesListViewHolder(
+            ListItemBinding.inflate(LayoutInflater.from(parent.context),
+                parent,false)
+        )
+        viewHolder.itemView.setOnClickListener {
+            val position = viewHolder.adapterPosition
+            onItemClicked(list[position])
+        }
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: MoviesListViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
