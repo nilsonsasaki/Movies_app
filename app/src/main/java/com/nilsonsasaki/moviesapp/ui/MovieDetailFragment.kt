19dfab58c9c3ad655ea450969f6224df8bbcf7ad
nilsonsasaki <nilsonsasaki@gmail.com>
2021-11-09 19:15:13 -0300

    updated MovieDetailFragment

--- app/src/main/java/com/nilsonsasaki/moviesapp/ui/MovieDetailFragment.kt
+++ app/src/main/java/com/nilsonsasaki/moviesapp/ui/MovieDetailFragment.kt
@@ -5,16 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
+import coil.load
 import com.nilsonsasaki.moviesapp.R
+import com.nilsonsasaki.moviesapp.databinding.FragmentMovieDetailBinding
+import com.nilsonsasaki.moviesapp.network.MovieDetails
+import com.nilsonsasaki.moviesapp.viewmodels.MovieTitleViewModel
 
 class MovieDetailFragment : Fragment() {
+
+    private var _binding: FragmentMovieDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private val navigationArgs: MovieDetailFragmentArgs by navArgs()
+    private val viewModel: MovieTitleViewModel by activityViewModels()
+
+    private lateinit var movieDetails: MovieDetails
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_detail, container, false)
+        _binding = FragmentMovieDetailBinding.inflate(inflater, container, false)
+        viewModel.getMovieDetails(navigationArgs.movieId)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.movieDetails.observe(this.viewLifecycleOwner) { movie ->
+            movieDetails = movie
+            bind(movieDetails)
+        }
+    }
+
+    private fun bind(movieDetails: MovieDetails) {
+        binding.tvDetailsTitle.text = movieDetails.title
+        binding.ivBackdrop.load(movieDetails.backdropUrl) {
+            error(R.drawable.ic_rate_star)
+        }
+        binding.tvRating.text = movieDetails.rating.toString()
+        binding.tvOverview.text = movieDetails.overview
+        binding.tvGenres.text = movieDetails.genres.toString()
+        binding.tvRuntime.text = movieDetails.runtime
+        binding.tvReleaseYear.text = movieDetails.date
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
