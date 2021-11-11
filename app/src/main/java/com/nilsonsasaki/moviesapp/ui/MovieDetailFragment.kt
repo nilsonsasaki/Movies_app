83351f35eb752ddb05806ad7f66ff4b43c64886d
nilsonsasaki <nilsonsasaki@gmail.com>
2021-11-11 11:29:36 -0300

    added auto retry connection

--- app/src/main/java/com/nilsonsasaki/moviesapp/ui/MovieDetailFragment.kt
+++ app/src/main/java/com/nilsonsasaki/moviesapp/ui/MovieDetailFragment.kt
@@ -1,17 +1,21 @@
 package com.nilsonsasaki.moviesapp.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ScrollView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import coil.load
+import com.google.android.material.progressindicator.CircularProgressIndicator
 import com.nilsonsasaki.moviesapp.R
 import com.nilsonsasaki.moviesapp.databinding.FragmentMovieDetailBinding
 import com.nilsonsasaki.moviesapp.network.MovieDetails
 import com.nilsonsasaki.moviesapp.viewmodels.MovieTitleViewModel
+import com.nilsonsasaki.moviesapp.viewmodels.MoviesAppApiStatus
 
 class MovieDetailFragment : Fragment() {
 
@@ -22,18 +26,48 @@ class MovieDetailFragment : Fragment() {
     private val viewModel: MovieTitleViewModel by activityViewModels()
 
     private lateinit var movieDetails: MovieDetails
+    private lateinit var movieDetailStatus: MoviesAppApiStatus
+    private var stopRetry = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentMovieDetailBinding.inflate(inflater, container, false)
-        viewModel.getMovieDetails(navigationArgs.movieId)
+        viewModel.setMovieId(navigationArgs.movieId)
+        viewModel.getMovieDetails()
+        viewModel.clearRetryCounter()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.movieDetailsStatus.observe(this.viewLifecycleOwner){status->
+            movieDetailStatus = status
+            when (movieDetailStatus) {
+                MoviesAppApiStatus.LOADING -> {
+                    binding.ivDetailsFragmentStatus.visibility = CircularProgressIndicator.VISIBLE
+                    binding.loadedDetailFragment.visibility=ScrollView.GONE
+                }
+                MoviesAppApiStatus.DONE -> {
+                    binding.ivDetailsFragmentStatus.visibility = CircularProgressIndicator.GONE
+                    binding.loadedDetailFragment.visibility = ScrollView.VISIBLE
+                }
+                else -> {
+                    viewModel.retryGetMoviesDetails()
+                }
+            }
+        }
+
+        viewModel.stopRetrying.observe(this.viewLifecycleOwner) { stop ->
+            stopRetry = stop
+            if(stopRetry){
+                val action = MovieDetailFragmentDirections.actionMovieDetailFragmentToErrorFragment()
+                this.findNavController().navigate(action)
+            }
+        }
+
         viewModel.movieDetails.observe(this.viewLifecycleOwner) { movie ->
             movieDetails = movie
             bind(movieDetails)
@@ -43,7 +77,8 @@ class MovieDetailFragment : Fragment() {
     private fun bind(movieDetails: MovieDetails) {
         binding.tvDetailsTitle.text = movieDetails.title
         binding.ivBackdrop.load(movieDetails.backdropUrl) {
-            error(R.drawable.ic_rate_star)
+            error(R.drawable.ic_broken_image)
+            placeholder(R.drawable.loading_animation)
         }
         binding.tvRating.text = movieDetails.rating.toString()
         binding.tvOverview.text = movieDetails.overview
@@ -54,6 +89,9 @@ class MovieDetailFragment : Fragment() {
 
     override fun onDestroy() {
         super.onDestroy()
+        viewModel.clearRetryCounter()
+        viewModel.clearMovieDetails()
+        viewModel.clearMovieDetailsStatus()
         _binding = null
     }
 
