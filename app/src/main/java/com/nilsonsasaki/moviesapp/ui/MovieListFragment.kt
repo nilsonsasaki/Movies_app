799f7ac4cf4063544ebd87f02565097833b57fc5
nilsonsasaki <nilsonsasaki@gmail.com>
2021-11-11 08:17:28 -0300

    added error handling

--- app/src/main/java/com/nilsonsasaki/moviesapp/ui/MovieListFragment.kt
+++ app/src/main/java/com/nilsonsasaki/moviesapp/ui/MovieListFragment.kt
@@ -9,9 +9,12 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.progressindicator.CircularProgressIndicator
 import com.nilsonsasaki.moviesapp.databinding.FragmentMoviesListBinding
 import com.nilsonsasaki.moviesapp.ui.adapters.MoviesListAdapter
 import com.nilsonsasaki.moviesapp.viewmodels.MovieTitleViewModel
+import com.nilsonsasaki.moviesapp.viewmodels.MoviesAppApiStatus
+import com.nilsonsasaki.moviesapp.viewmodels.MoviesAppScreen
 
 class MovieListFragment : Fragment() {
 
@@ -19,14 +22,17 @@ class MovieListFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var recyclerView: RecyclerView
+    lateinit var movieListStatus: MoviesAppApiStatus
 
+    private var stopRetry: Boolean = false
     private val viewModel: MovieTitleViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
+        viewModel.setAppScreen(MoviesAppScreen.LIST)
         _binding = FragmentMoviesListBinding.inflate(layoutInflater, container, false)
         return (binding.root)
     }
@@ -34,11 +40,36 @@ class MovieListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel.movieListStatus.observe(this.viewLifecycleOwner) { status ->
+            movieListStatus = status
+            when (movieListStatus) {
+                MoviesAppApiStatus.LOADING -> {
+                    binding.rvMoviesList.visibility = RecyclerView.GONE
+                    binding.piDetailsFragmentStatus.visibility = CircularProgressIndicator.VISIBLE
+                }
+                MoviesAppApiStatus.DONE -> {
+                    binding.rvMoviesList.visibility = RecyclerView.VISIBLE
+                    binding.piDetailsFragmentStatus.visibility = CircularProgressIndicator.GONE
+                }
+                else -> {
+                    viewModel.retryGetMoviesList()
+                }
+            }
+        }
+        viewModel.stopRetrying.observe(this.viewLifecycleOwner) { stop ->
+            stopRetry = stop
+            if(stopRetry){
+                val action = MovieListFragmentDirections.actionMoviesListFragmentToErrorFragment()
+                this.findNavController().navigate(action)
+            }
+        }
+
         recyclerView = binding.rvMoviesList
         recyclerView.layoutManager = LinearLayoutManager(context)
         viewModel.moviesList.observe(this.viewLifecycleOwner) { movie ->
-            recyclerView.adapter = MoviesListAdapter(movie){
-                val action = MovieListFragmentDirections.actionMoviesListFragmentToMovieDetailFragment(it.id)
+            recyclerView.adapter = MoviesListAdapter(movie) {
+                val action =
+                    MovieListFragmentDirections.actionMoviesListFragmentToMovieDetailFragment(it.id)
                 this.findNavController().navigate(action)
             }
         }
