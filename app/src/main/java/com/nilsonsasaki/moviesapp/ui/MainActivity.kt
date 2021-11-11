01665562d53490e883823b134a11ca9ead3d09aa
nilsonsasaki <nilsonsasaki@gmail.com>
2021-11-11 11:28:16 -0300

    added error fragment to top-level destination

--- app/src/main/java/com/nilsonsasaki/moviesapp/ui/MainActivity.kt
+++ app/src/main/java/com/nilsonsasaki/moviesapp/ui/MainActivity.kt
@@ -2,9 +2,9 @@ package com.nilsonsasaki.moviesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.nilsonsasaki.moviesapp.R
 import com.nilsonsasaki.moviesapp.databinding.ActivityMainBinding
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private val appBarConfiguration = AppBarConfiguration(setOf(R.id.moviesListFragment, R.id.errorFragment))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +25,7 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
